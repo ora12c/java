5677170bf6368669b92e40c8c68211da2d8631fa
ora12c <oracle_dba@mail.ru>
2014-12-27 20:07:15 +0300

    first03

--- first/HelloJava.java
+++ first/HelloJava.java
@@ -1,7 +1,10 @@
+import javax.swing.*;
 public class HelloJava 
 {
   public static void main( String[] args ) {
-    javax.swing.JFrame frame = new javax.swing.JFrame( "Hello, Java!" );
+    JFrame frame = new JFrame( "HelloJava" );
+	JLabel label = new JLabel("Hello, Java!", JLabel.CENTER );
+    frame.getContentPane().add( label );
     frame.setSize( 300, 300 );
     frame.setVisible( true );
   }
