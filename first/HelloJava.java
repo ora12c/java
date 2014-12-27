5b7de33f3823de4fce85f7554e38a94727e7c839
ora12c <oracle_dba@mail.ru>
2014-12-27 20:19:44 +0300

    first06

--- first/HelloJava.java
+++ first/HelloJava.java
@@ -14,26 +14,53 @@ public class HelloJava
 }
 
 class HelloComponent extends JComponent 
-	implements MouseMotionListener 
+	implements MouseMotionListener, ActionListener
 {
   String theMessage;
-  int messageX = 125, messageY = 95; // Coordinates of the message
+  int messageX = 125, messageY = 95;  // Coordinates of the message
+
+  JButton theButton;
+
+  int colorIndex;  // Current index into someColors
+  static Color[] someColors = { 
+	Color.black, Color.red, Color.green, Color.blue, Color.magenta };
 
   public HelloComponent( String message ) {
     theMessage = message;
-    addMouseMotionListener(this);
+    theButton = new JButton("Change Color");
+    setLayout( new FlowLayout() );
+    add( theButton );
+    theButton.addActionListener( this );
+    addMouseMotionListener( this );
   }
 
   public void paintComponent( Graphics g ) {
     g.drawString(theMessage, messageX, messageY);
   }
 
-  public void mouseDragged(MouseEvent e) {
-    // Save the mouse coordinates and paint the message.
+  public void mouseDragged( MouseEvent e ) {
     messageX = e.getX();
     messageY = e.getY();
     repaint();
   }
 
-  public void mouseMoved(MouseEvent e) { }
+  public void mouseMoved( MouseEvent e ) {}
+
+  public void actionPerformed( ActionEvent e ) {
+    // Did somebody push our button?
+    if (e.getSource() == theButton)
+      changeColor();
+  }
+
+  synchronized private void changeColor() {
+    // Change the index to the next color, awkwardly.
+    if (++colorIndex == someColors.length)
+      colorIndex = 0;
+    setForeground(currentColor(  )); // Use the new color.
+    repaint(  ); // Paint again so we can see the change.
+  }
+
+  synchronized private Color currentColor() {
+    return someColors[colorIndex];
+  }
 }
