5bafed9e353d9183ed8f0c856eff1b42016c050e
ora12c <oracle_dba@mail.ru>
2014-12-27 20:04:17 +0300

    first02

--- first/HelloJava.java
+++ first/HelloJava.java
@@ -1,7 +1,9 @@
 public class HelloJava 
 {
   public static void main( String[] args ) {
-	System.out.println("Hello, Java!");
+    javax.swing.JFrame frame = new javax.swing.JFrame( "Hello, Java!" );
+    frame.setSize( 300, 300 );
+    frame.setVisible( true );
   }
 }
 
