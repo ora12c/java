f2b161d4161f7258880551b07ccc74a2962f0d8c
ora12c <oracle_dba@mail.ru>
2014-12-27 20:16:23 +0300

    first05

--- first/HelloJava.java
+++ first/HelloJava.java
@@ -1,15 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-public class HelloJava 
+
+public class HelloJava
 {
   public static void main( String[] args ) {
     JFrame frame = new JFrame( "HelloJava" );
-    frame.add( new HelloComponent() );
+    frame.add( new HelloComponent("Hello Java!") );
+	frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
     frame.setSize( 300, 300 );
     frame.setVisible( true );
   }
 }
-class HelloComponent extends JComponent {
-  public void paintComponent( java.awt.Graphics g ) {
-    g.drawString( "Hello, Java!", 125, 95 );
+
+class HelloComponent extends JComponent 
+	implements MouseMotionListener 
+{
+  String theMessage;
+  int messageX = 125, messageY = 95; // Coordinates of the message
+
+  public HelloComponent( String message ) {
+    theMessage = message;
+    addMouseMotionListener(this);
   }
+
+  public void paintComponent( Graphics g ) {
+    g.drawString(theMessage, messageX, messageY);
+  }
+
+  public void mouseDragged(MouseEvent e) {
+    // Save the mouse coordinates and paint the message.
+    messageX = e.getX();
+    messageY = e.getY();
+    repaint();
+  }
+
+  public void mouseMoved(MouseEvent e) { }
 }
